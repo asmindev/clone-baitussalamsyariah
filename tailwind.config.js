36448163f7a6db3186fd38e1510bf7a720fb0857
asmindev <asmin987asmin@gmail.com>
2022-06-02 14:05:53 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        bebas: ['Montserrat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
